29b39515679e8bf8662c31cbe960d123b43b7c47
Mona Lisa <gorankn89@gmail.com>
2021-10-12 14:51:54 +0200

    second commit

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to cars</title>
+    <title>About</title>
 </head>
 <body>
-    <div>
-    <h1>Welcome to cars</h1>   
-    </div> 
+  <div>
+      <h1>
+       About US   
+    </h1>
+</div>  
 </body>
 </html>
